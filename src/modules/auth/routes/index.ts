054b760dca1db2be9c39ed0ef51c8ab1a902ed17
Aldi Abdu Malik <aldiabdumalik98@gmail.com>
2025-03-14 09:35:34 +0700

    feat: set route auth with handle error

---
+++ src/modules/auth/routes/index.ts
@@ -0,0 +1,29 @@
+import { globalError, validationError } from "@/helpers/errors";
+import type { ResponseError, ValidationType } from "@/helpers/models";
+import Elysia from "elysia";
+
+const authRoutes = new Elysia({ prefix: "/auth" });
+
+authRoutes.onError(({ error, code, ...ctx }): ResponseError => {
+	if ("error" in error && "code" in error) {
+		const responseValidationError = validationError(
+			error as ValidationType,
+			ctx,
+		);
+		if (responseValidationError) return responseValidationError;
+	}
+
+	if (error instanceof Error) {
+		const responseGlobalError = globalError(error, ctx);
+		if (responseGlobalError) return responseGlobalError;
+	}
+
+	return {
+		statusCode: 500,
+		success: false,
+		message: "message" in error ? error.message : "Internal Server Error",
+		error: {},
+	};
+});
+
+export default authRoutes;
